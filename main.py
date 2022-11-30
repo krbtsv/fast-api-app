7cfd1dbd86c5a7e75cb64e2226f2cd87ca7f2776
krbtsv <maxim.karabtsov@gmail.com>
2022-12-01 02:48:05 +0300

    Created endpoints for create and get records from DB.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import uvicorn
+from envparse import Env
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi.routing import APIRoute, APIRouter
+from starlette.requests import Request
 
 app = FastAPI()
+
+env = Env()
+MONGODB_URL = env.str("MONGODB_URL", default="mongodb://localhost:27017/test_database")
 
 
 async def ping() -> dict:
@@ -13,12 +19,32 @@ async def mainpage() -> str:
     return "YOU ARE ON THE MAIN PAGE"
 
 
+async def create_record(request: Request) -> dict:
+    mongo_client: AsyncIOMotorClient = request.app.state.mongo_client["test_database"]
+    await mongo_client.records.insert_one({"sample": "record"})
+    return {"Success": True}
+
+
+async def get_records(request: Request) -> list:
+    mongo_client: AsyncIOMotorClient = request.app.state.mongo_client["test_database"]
+    cursor = mongo_client.records.find({})
+    res = list()
+    for document in await cursor.to_list(length=100):
+        document["_id"] = str(document["_id"])
+        res.append(document)
+    return res
+
+
 routes = [
     APIRoute(path="/ping", endpoint=ping, methods=["GET"]),
-    APIRoute(path="/", endpoint=mainpage, methods=["GET"])
+    APIRoute(path="/", endpoint=mainpage, methods=["GET"]),
+    APIRoute(path="/create_record", endpoint=create_record, methods=["POST"]),
+    APIRoute(path="/get_records", endpoint=get_records, methods=["GET"]),
 ]
 
 app.include_router(APIRouter(routes=routes))
+client = AsyncIOMotorClient(MONGODB_URL)
+app.state.mongo_client = client
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
